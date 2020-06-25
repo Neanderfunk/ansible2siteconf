4d68b9a59709dcd9f67ced3659887aea61c59a52
Alex <af@kreativstrecke.de>
2020-06-25 21:33:18 +0200

    port eingebaut. branch auf stable.

--- generate.py
+++ generate.py
@@ -8,7 +8,7 @@ import yaml
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def render(withkey, id, name, shortname, hostname_prefix, seed, v4net, v6net, lon, lat, zoom, wifi24channel, htmode24,wifi5channel,htmode5, nextnode4, nextnode6, authorized_keys):
+def render(withkey, id, name, shortname, hostname_prefix, seed, port, v4net, v6net, lon, lat, zoom, wifi24channel, htmode24,wifi5channel,htmode5, nextnode4, nextnode6, authorized_keys):
     j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
                          trim_blocks=True)
     return j2_env.get_template('template_site.j2').render(
@@ -18,6 +18,7 @@ def render(withkey, id, name, shortname, hostname_prefix, seed, v4net, v6net, lo
         shortname=shortname,
         hostname_prefix=hostname_prefix,
         seed=seed,
+        port=port,
         v4net=v4net,
         v6net=v6net,
         fastdpeers=fastdpeers,
@@ -95,12 +96,12 @@ if __name__ == '__main__':
             if not os.path.exists(THIS_DIR + '/out/' + shortname):
                 os.mkdir(THIS_DIR + '/out/' + shortname)
             with open(THIS_DIR + '/out/' + shortname + '/site.conf', 'w') as f:
-                f.write(render(False, int(id), name, shortname, hostname_prefix, seed, v4net, v6net, lon, lat, zoom, wifi24channel, htmode24, wifi5channel, htmode5, nextnode4, nextnode6, authorized_keys))
+                f.write(render(False, int(id), name, shortname, hostname_prefix, seed, port, v4net, v6net, lon, lat, zoom, wifi24channel, htmode24, wifi5channel, htmode5, nextnode4, nextnode6, authorized_keys))
 
             if not os.path.exists(THIS_DIR + '/out/' + shortname + '-key'):
                 os.mkdir(THIS_DIR + '/out/' + shortname + '-key')
             with open(THIS_DIR + '/out/' + shortname + '-key/site.conf', 'w') as f:
-                f.write(render(True, int(id), name, shortname, hostname_prefix, seed, v4net, v6net, lon, lat, zoom, wifi24channel, htmode24, wifi5channel, htmode5, nextnode4, nextnode6, authorized_keys))
+                f.write(render(True, int(id), name, shortname, hostname_prefix, seed, port, v4net, v6net, lon, lat, zoom, wifi24channel, htmode24, wifi5channel, htmode5, nextnode4, nextnode6, authorized_keys))
 
     # for id, values in domains.items():
     #     names = values['names']
